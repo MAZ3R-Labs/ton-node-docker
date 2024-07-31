a7b2df88d1e0b936f6ed6059f794963863688f5e
mazer <mazer@fdc.ai>
2024-07-31 16:51:29 +0800

    copy api config

--- setup_api_key.sh
+++ setup_api_key.sh
@@ -5,3 +5,5 @@ source .env
 docker compose build ton-api
 NODE_API_KEY=$(docker run --rm -v $API_CONF_VOLUME:/conf -v $NODE_STATE_VOLUME:/liteserver ton-api -c "python /conf/generate-api-key.py")
 sed -i "s~NODEAPIKEY~$NODE_API_KEY~g" ${API_CONF_VOLUME}/${API_NETWORK}-config-${API_MODE}.json
+
+cp ${API_CONF_VOLUME}/${API_NETWORK}-config-${API_MODE}.json .
