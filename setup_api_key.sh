b56ba7db83ace934940a56eba46699f7aaff025b
mazer <mazer@fdc.ai>
2024-07-31 17:20:24 +0800

    modify ip

--- setup_api_key.sh
+++ setup_api_key.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 export TON_NODE_IP=$(curl -s https://ipinfo.io/ip)
+
+ip2dec () {
+    local a b c d ip=$@
+    IFS=. read -r a b c d <<< "$ip"
+    printf '%d\n' "$((a * 256 ** 3 + b * 256 ** 2 + c * 256 + d))"
+}
+
 source .env
 docker compose build ton-api
 NODE_API_KEY=$(docker run --rm -v $API_CONF_VOLUME:/conf -v $NODE_STATE_VOLUME:/liteserver ton-api -c "python /conf/generate-api-key.py")
-sed -i "s~NODEAPIKEY~$NODE_API_KEY~g" ${API_CONF_VOLUME}/${API_NETWORK}-config-${API_MODE}.json
-
+IP_DEC=$(ip2dec ${TON_NODE_IP})
 cp ${API_CONF_VOLUME}/${API_NETWORK}-config-${API_MODE}.json .
+sed -i "s~NODEAPIKEY~$NODE_API_KEY~g" ./${API_NETWORK}-config-${API_MODE}.json
+sed -i "s~2886860802~$IP_DEC~g" ./${API_NETWORK}-config-${API_MODE}.json
